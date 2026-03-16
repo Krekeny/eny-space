b65a73720051e3eef4d621422eec58eb3563669f
Davor Culibrk <davor.culibrk@krekeny.com>
2026-03-16 16:54:06 +0100

    fix(hero): remove doubled layer

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import { HeroBackground } from "./hero-background";
 import { HeroLeft } from "./hero-left";
 import { HeroRight } from "./hero-right";
 
 export function Hero() {
   return (
     <section className="relative flex min-h-[calc(100vh-3.5rem)] w-full flex-col justify-end overflow-hidden px-4 pb-12 pt-16 sm:px-6 sm:pt-24">
-      <HeroBackground />
       {/* Centered hero logo, moved higher with -mt-24 (negative margin top) */}
       <div
         className="pointer-events-none absolute inset-0 flex items-center justify-center opacity-15 -mt-24"
